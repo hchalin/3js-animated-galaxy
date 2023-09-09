0512ca766f6b2c572ce6c6036c7d15ac9a42f5d5
Hayden <hchalin98@gmail.com>
2023-09-09 09:45:52 -0700

    adding randomness to the scale of each point

---
+++ src/shaders/galaxy/vertex.glsl
@@ -0,0 +1,17 @@
+uniform float uSize;
+
+ void main(){
+   /**
+  * Position
+  */
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectedPosition;
+
+  /**
+  * Points
+  */
+  gl_PointSize = uSize;
+ }
